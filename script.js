77185a9f249390a7d7b07d4f7f40d12630262b54
Kylie <kylie.conner93@gmail.com>
2024-01-30 17:21:11 -0500

    Assigned stick to the inventory variable.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let monsterHealth; //Declared monsterHealth and inventory as variables, but haven't initialized them.
 let xp = 0;
 let health = 100;
-let inventory;
 let gold = 50;
 let currentWeapon = 0;
 let fighting;
+let monsterHealth; 
+let inventory = "stick"; //Assigned "stick" to the inventory variable.
