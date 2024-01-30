c7566bdbc8787921a8957b4f6b29fe58d42d22ec
Kylie <kylie.conner93@gmail.com>
2024-01-30 18:14:05 -0500

    Changed button1 variable to const since I won't be reassigning it. Also noticed I can't comment the usual way on the html file, but CAN on the js file. I think it's because the js is running through the html file.

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth; 
 let inventory = ["stick"]; 
-let button1 = document.querySelector("#button1"); 
+const button1 = document.querySelector("#button1"); //Changed it to const because I'm not going to reassign this variable. This way,
+//it will throw an error if I do accidently try reassigning it. Also interesting thing, I can't comment the usual way in the html
+//file, but it is working on this js file. My theory is that it's because the js is RUNNING through the html and reads everything?
