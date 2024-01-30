7d83f9a303709d580f135e0765de79e3cc4a3957
Kylie <kylie.conner93@gmail.com>
2024-01-30 18:18:09 -0500

    Added 2 more const variables that are referring to their similar id's in the html.

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth; 
 let inventory = ["stick"]; 
-const button1 = document.querySelector("#button1"); //Changed it to const because I'm not going to reassign this variable. This way,
-//it will throw an error if I do accidently try reassigning it. Also interesting thing, I can't comment the usual way in the html
-//file, but it is working on this js file. My theory is that it's because the js is RUNNING through the html and reads everything?
+const button1 = document.querySelector("#button1"); 
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3"); //Added 2 more const variables that are referring to their similar id's in the html.
