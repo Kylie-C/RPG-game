d401e2b8ef1123f2123e5a6269423820ef4d8977
Kylie <kylie.conner93@gmail.com>
2024-01-30 18:25:18 -0500

    used a selector list (#controls, #stats {}) to give both id's the same styling

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ let monsterHealth;
 let inventory = ["stick"]; 
 const button1 = document.querySelector("#button1"); 
 const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3"); //Added 2 more const variables that are referring to their similar id's in the html.
+const button3 = document.querySelector("#button3"); 
