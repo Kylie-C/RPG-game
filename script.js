2fc092757294b3b4696b2d819945886f915e1e09
Kylie <kylie.conner93@gmail.com>
2024-01-30 17:24:59 -0500

    Changed the inventory variable to be an array so the player's inventory can hold multiple items.

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth; 
-let inventory = "stick"; //Assigned "stick" to the inventory variable.
+let inventory = ["stick", "dagger", "sword"]; //The player's inventory in my game will be able to hold multiple items. I changed the string to an array because it can hold multiple values. 
