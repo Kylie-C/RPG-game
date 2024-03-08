3d306ad521f56a68b589fb38dd2f977bb39506f9
Kylie <kylie.conner93@gmail.com>
2024-03-08 10:58:22 -0500

    Added an if statement to check if health is less than or equal to 0, then it will call the lose () function, which hasn't been created yet.

--- script.js
+++ script.js
@@ -150,8 +150,12 @@ function attack (){
   monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
+  if (health <= 0){
+    lose();
+  }
 }
-// Updated the healthText.innerText to equal health. Also updated the monsterHealthText.innerText to equal monsterHealth.
+// Added an if statement to check if health is less than or equal to 0, then it will call the lose () function, which hasn't been
+// created yet.
 function dodge (){
 }
 
